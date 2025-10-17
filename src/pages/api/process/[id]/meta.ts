4337dcb3eb60041564971bef45c3aa4da59097e9
Yesid Gonzalez <oyquintero@rbm.com.co>
2025-10-17 07:57:26 -0500

    console url server

--- src/pages/api/process/[id]/meta.ts
+++ src/pages/api/process/[id]/meta.ts
@@ -3,14 +3,15 @@ import { URL_SERVER } from 'astro:env/server'
 
 export const GET: APIRoute = async ({params}) => {
     const { id } = params
-    console.log({id})
+    console.log({id, URL_SERVER})
     const response = await fetch(`${URL_SERVER}/api/user/${id}/meta`, {
         headers: {
             'Content-Type': 'application/json',
         },
     })
+    console.log(response)
     const res = await response.json()
-    console.log(res)
+    console.log({res})
     return new Response(JSON.stringify(res), {
         headers: {
             'Content-Type': 'application/json',
