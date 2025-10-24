abb35e5b5f6669a91d08d47f1f47d5d621a784ae
Yesid Gonzalez <oyquintero@rbm.com.co>
2025-10-24 16:20:22 -0500

    feat: Add onboarding components for digital identity process
    - Implemented Boarding component to handle user identity creation with a selfie and ID.
    - Created DesktopPage component to display a QR code for mobile access.
    - Developed MobilePage component to integrate onboarding visuals and process loading state.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  devToolbar: {
+    enabled: false,
+  },
   env: {
     schema: {
       URL_SERVER: envField.string({context: 'server', access: 'public', default: 'https://poc-registration-services-production.up.railway.app' })
